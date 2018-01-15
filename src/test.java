6519f14211029881dbe00cf7fb097416cc7da113
ngsm <ngsm@5CG6352D49.STAFF.HELP.LOCAL>
2018-01-15 11:12:30 +0800

    Lecture 1 Worksheet

--- src/test.java
+++ src/test.java
@@ -1,33 +1,62 @@
+
 /**
  * A program to find and display the weekly wage of a worker,
  * the data is to be entered interactively by an administrative clerk – use the Scanner class.
+ *
  * @author ngsm
  */
 import java.util.Scanner;
 
 public class test {
 
-    public static void main(String[] args) {
+    final static double HOURLYRATE = 12.50;
+    static final double OVERTIMERATE = 1.5;
+    static final int OVERTIMERANGE = 40;
+    static Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-        final double HOURLYRATE = 12.50;
-        final double OVERTIMERATE = 1.5;
-        final int OVERTIMERANGE = 40;
-        
+    public static void main(String[] args) {
+        System.out.print("Enter number of workers  :");
+        int numWorkers = sc.nextInt();
+        double total = 0.0;
+        for (int i = 1; i <= numWorkers; i++) { 
+            
+            total += getWorkerWage(i);
+            
+        }
+
+        System.out.printf("The total wages paid is RM%5.2f%n", total);
+
+    }
+
+    /**
+     * A method to get info about one worker
+     *
+     */
+    public static double getWorkerWage(int workerNum) {
         // prompt user for hours worked
-        System.out.print("Enter the hours worked :");
+        System.out.print("Enter the hours worked for worker " + workerNum + ": ");
         int hoursWorked = sc.nextInt();
+        double weeklyWage = calculateWage(hoursWorked);
+
+        System.out.printf("The weekly wage for worker %d is RM%5.2f%n", workerNum, weeklyWage);
+        return weeklyWage;
+    }
+
+    /**
+     * A method to calculate the weekly wage given the hours worked
+     *
+     * @param hours
+     * @return the weekly wage for a worker
+     */
+    public static double calculateWage(int hours) {
         double weeklyWage;
-        
-        if (hoursWorked > OVERTIMERANGE)
-        {
-            int extraHours = hoursWorked - OVERTIMERANGE;
+        if (hours > OVERTIMERANGE) {
+            int extraHours = hours - OVERTIMERANGE;
             double extraPay = extraHours * HOURLYRATE * OVERTIMERATE;
             weeklyWage = HOURLYRATE * OVERTIMERANGE + extraPay;
+        } else {
+            weeklyWage = HOURLYRATE * hours;
         }
-        else
-            weeklyWage = HOURLYRATE * hoursWorked;
-        System.out.printf("The weekly wage is RM%5.2f%n",  weeklyWage);
-
+        return weeklyWage;
     }
 }
