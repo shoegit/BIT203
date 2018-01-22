b97b419546bc6d3d60fb51ebe486eaac33c76116
ngsm <ngsm@5CG6352D49.STAFF.HELP.LOCAL>
2018-01-22 11:13:52 +0800

    Lecture 2 Add Project

--- src/TPC/TPC.java
+++ src/TPC/TPC.java
@@ -1,37 +1,75 @@
-
 package TPC;
 
 /**
  * TPC represents a Company (The Project Company)
+ *
  * @author ngsm
  */
 public class TPC {
-    
+
     private int numberOfProjects;
     private Project[] tpcProjects;
     private final int MAX = 5;         // only 50 projects allowed!!
-    
-    /** 
+
+    /**
      * Constructor to initialize the projects array
      */
-    public TPC()
-    {
+    public TPC() {
         tpcProjects = new Project[MAX];
         numberOfProjects = 0;
     }
-    
+
     /**
      * add a project to TPC
+     *
      * @param projectName name of the new project
      * @return the project that was created, null if not created.
      */
-    public Project addProject(String projectName)
-    {
-        if (tpcProjects.length== numberOfProjects)   // already full
+    public Project addProject(String projectName) {
+        if (tpcProjects.length == numberOfProjects) // already full
+        {
             return null;
+        }
         Project newProj = new Project(projectName);
         tpcProjects[numberOfProjects++] = newProj;
         return newProj;         // return the project that was created
-        
+
     }
+
+    /* findProject() method to return a project based on a project number.
+     */
+    public Project findProject(int projectNum) {
+
+        for (int i = 0; i < numberOfProjects; i++) {
+            if (tpcProjects[i].getProjectNum() == projectNum) {
+                return tpcProjects[i];
+            }
+        }
+        return null;
+    }
+
+    /* findProject() method to return a list of project numbers and names which 
+	 * match a keyword entered.
+     */
+    public String findProject(String searchString) {
+        String result = "Projects containing : '" + searchString + "'\n";
+        for (int i = 0; i < numberOfProjects; i++) {
+            if (tpcProjects[i].getProjectName().contains(searchString)) {
+                result += tpcProjects[i].getProjectNum() + " " + tpcProjects[i].getProjectName();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * showAllProjects() method to return a list of all project numbers and names  
+     */
+    public String showAllProjects() {
+        String result = "All Projects:";
+        for (int i = 0; i < numberOfProjects; i++) {
+            result += tpcProjects[i].getProjectNum() + " " + tpcProjects[i].getProjectName();
+        }
+        return result;
+    }
+
 }
