a249a8cf4793bafd5aa50bd14eea7a0d08d08015
ngsm <ngsm@5CG6352D49.STAFF.HELP.LOCAL>
2018-01-22 10:39:45 +0800

    Lecture 2 Add Project

---
+++ src/TPC/TPC.java
@@ -0,0 +1,37 @@
+
+package TPC;
+
+/**
+ * TPC represents a Company (The Project Company)
+ * @author ngsm
+ */
+public class TPC {
+    
+    private int numberOfProjects;
+    private Project[] tpcProjects;
+    private final int MAX = 50;         // only 50 projects allowed!!
+    
+    /** 
+     * Constructor to initialize the projects array
+     */
+    public TPC()
+    {
+        tpcProjects = new Project[MAX];
+        numberOfProjects = 0;
+    }
+    
+    /**
+     * add a project to TPC
+     * @param projectName name of the new project
+     * @return the project that was created, null if not created.
+     */
+    public Project addProject(String projectName)
+    {
+        if (tpcProjects.length== numberOfProjects)   // already full
+            return null;
+        Project newProj = new Project(projectName);
+        tpcProjects[numberOfProjects++] = newProj;
+        return newProj;         // return the project that was created
+        
+    }
+}
---
+++ src/TPC/TPCConsole.java
@@ -0,0 +1,16 @@
+package TPC;
+
+/**
+ * User interface for TPC
+ * @author ngsm
+ */
+import java.util.*;
+public class TPCConsole {
+    
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args)
+    {
+        
+    }
+}
